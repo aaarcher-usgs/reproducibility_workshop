76308a206afee381475ea4a081951ceec0d4394c
aaarchmiller <althea.archmiller@gmail.com>
2017-07-24 13:07:58 -0500

    activity 1 for DNR workshop

---
+++ student_folders/althea/activity1a_data_processing.R
@@ -0,0 +1,25 @@
+# RIKZ Data Loading and Processing
+
+## Preamble 
+remove(list=ls())
+
+### Step 1. Load data from csv
+rikz.data.1 <- read.csv("data/raw_data/RIKZ_1.csv")
+rikz.data.2 <- read.csv("data/raw_data/RIKZ_2.csv")
+rikz.data.3 <- read.csv("data/raw_data/RIKZ_3.csv")
+rikz.data.4 <- read.csv("data/raw_data/RIKZ_4.csv")
+
+### Step 2. Combine datasets together
+rikz.data <- rbind(rikz.data.1,
+                   rikz.data.2,
+                   rikz.data.3,
+                   rikz.data.4)
+table(rikz.data$week)
+str(rikz.data)
+
+### Step 3. Data processing
+# Convert Beach to categorical
+rikz.data$Beach <- as.character(rikz.data$Beach)
+
+### Step 4. Save the processed data
+save(rikz.data, file = "data/processed_data/rikz.Rdata")
---
+++ student_folders/althea/activity1b_data_analysis.R
@@ -0,0 +1,19 @@
+# Linear Regression of RIKZ data
+
+## Preamble 
+
+# Load libraries
+
+# Clear environment
+remove(list=ls())
+
+### Step 1. Load data
+load(file = "data/processed_data/rikz.Rdata")
+
+### Step 2. Linear regression
+lm.out <- lm(Richness~NAP, data = rikz.data)
+summary(lm.out)
+
+### Step 3. Plot relationship
+plot(rikz.data$NAP, rikz.data$Richness)
+abline(lm.out)
